f9bafddcee5becc61df12ef90c8130b1cb19389c
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-04-12 16:57:22 +0700

    commit

--- 4.Python_loop_exercises.py
+++ 4.Python_loop_exercises.py
@@ -2,7 +2,7 @@
 # Exercise 1: Print the sum of the current number and the previous number
 # Write a program to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number.
 # Expected result: 0,1,3,5,7,9,11,13,15,17
-prev_num=0
+prev_num = 0
 for i in range(0,11):
     x = i + prev_num
     prev_num = i
